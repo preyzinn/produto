e570a1b6a50405b122f7dcccd1e9f150a78e732b
Ian <ianaraujo47@gmail.com>
2025-02-26 23:57:44 -0300

    Mano nao sei oq eu to fazendo eu esqueci tudo

---
+++ produto java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o nome do produto: ");
+		String nome = scanner.nextLine();
+		System.out.println("Digite a descrição do produto: ");
+		String descricao = scanner.nextLine();
+		System.out.println("Digite o preço do produto: ");
+		double preco = scanner.nextDouble();
+		System.out.println("Digite a quantidade do produto: ");
+		int quantidade = scanner.nextInt();
+		Produto produto = new Produto(preco, nome, descricao, quantidade);
+		System.out.println("Produto cadastrado com sucesso!");
+		System.out.println("Nome: " + produto.nome);
+		System.out.println("Descrição: " + produto.descricao);
+
+		System.out.println("Preço: " + produto.preco);
+		System.out.println("Quantidade: " + produto.quantidade);
+		
+	}
+}
---
+++ produto java/Produto.java
@@ -0,0 +1,13 @@
+public class Produto{
+    double preco;
+    String nome;
+    String descricao;
+    int quantidade;
+    public Produto(double preco, String nome, String descricao, int quantidade){
+        this.preco = preco;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+
+    }
+}
